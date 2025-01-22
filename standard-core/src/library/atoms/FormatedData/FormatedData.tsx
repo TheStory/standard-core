e2875e7e3c4fbcc03ad90336d01a59fc9d7d2e23
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2025-01-22 16:18:45 +0100

    add FormatedData component

---
+++ standard-core/src/library/atoms/FormatedData/FormatedData.tsx
@@ -0,0 +1,23 @@
+import Box from "@mui/material/Box";
+import { useFormatter, useTranslations } from "next-intl";
+
+const FormattedDate = ({ value }: { value?: Date | string }) => {
+  const t = useTranslations("common");
+  const f = useFormatter();
+
+  const formatDate = () => {
+    if (value === undefined) return "-";
+
+    return f.dateTime(new Date(value), {
+      dateStyle: "medium",
+    });
+  };
+
+  return (
+    <Box component="span" sx={{ whiteSpace: "nowrap" }}>
+      {formatDate()} {t("year")}
+    </Box>
+  );
+};
+
+export default FormattedDate
