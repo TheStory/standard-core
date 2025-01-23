611f45cf93023aff46ecffb92dbe2179d6d9f575
jakubwozniak <kwozniak992@gmail.com>
2025-01-23 13:16:12 +0100

    [SW-76]: Add Breadcrumbs stories

---
+++ standard-core/src/stories/Atoms/Breadcrumbs/Breadcrumbs.stories.ts
@@ -0,0 +1,52 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Breadcrumbs } from "standard-core/atoms/Breadcrumbs";
+
+const meta: Meta<typeof Breadcrumbs> = {
+  title: "Atoms/Breadcrumbs",
+  component: Breadcrumbs,
+  tags: ["autodocs"],
+  argTypes: {
+    crumbs: { control: "object" },
+    sx: { control: "object" },
+    lightVariant: { control: "boolean" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    crumbs: [
+      { link: "Home", href: "/" },
+      { link: "Category", href: "category" },
+      { link: "Subcategory", href: "subcategory" },
+    ],
+    sx: {},
+    lightVariant: false,
+  },
+};
+
+export const LightVariant: Story = {
+  args: {
+    crumbs: [
+      { link: "Home", href: "/" },
+      { link: "Category", href: "category" },
+      { link: "Subcategory", href: "subcategory" },
+    ],
+    sx: {},
+    lightVariant: true,
+  },
+};
+
+export const CustomStyle: Story = {
+  args: {
+    crumbs: [
+      { link: "Home", href: "/" },
+      { link: "Category", href: "category" },
+      { link: "Subcategory", href: "subcategory" },
+    ],
+    sx: { color: "red" },
+    lightVariant: false,
+  },
+};
