701f4358904ad9112460d88b22e8bf792ff741a4
jakubwozniak <kwozniak992@gmail.com>
2025-02-11 18:56:27 +0100

    [SW-230]: add Pagination stories

---
+++ standard-core/src/stories/Atoms/Pagination/Pagination.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Pagination from "standard-core/atoms/Pagination/Pagination";
+
+const meta: Meta<typeof Pagination> = {
+  title: "Atoms/Pagination",
+  component: Pagination,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  argTypes: {
+    pageCount: {
+      control: { type: "number", min: 1, max: 20 },
+      description: "Total number of pages.",
+    },
+    sx: {
+      control: "object",
+      description: "Styles for the pagination component.",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Pagination>;
+
+export const Default: Story = {
+  args: {
+    pageCount: 5,
+  },
+};
