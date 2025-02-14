bb4b35e686b15a7189c8e2a3e786506c71f349ad
jakubwozniak <kwozniak992@gmail.com>
2025-02-14 16:22:43 +0100

    SW-76: Add CroppedImage stories

---
+++ standard-core/src/stories/Atoms/CroppedImage/CroppedImage.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CroppedImage from "standard-core/atoms/CroppedImage/CroppedImage";
+
+const meta: Meta<typeof CroppedImage> = {
+  title: "Atoms/CroppedImage",
+  component: CroppedImage,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    width: { control: "number" },
+    height: { control: "number" },
+    cover: { control: "boolean" },
+    resizingType: {
+      control: "select",
+      options: ["fill", "fit", "fill-down", "force", "auto"],
+    },
+  },
+  args: {
+    width: 486,
+    height: 672,
+    src: "https://storybook.js.org/images/placeholders/350x150.png",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
