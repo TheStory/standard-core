f86ad0f3e1821e136fd068070b728a68f45e986c
Yaroslav Shatkevich <jarek@thestory.pl>
2025-03-09 15:00:16 +0100

    Add ContrastAware interface

--- standard-core/src/library/types/index.ts
+++ standard-core/src/library/types/index.ts
@@ -1,4 +1,8 @@
 export type ImageResizeOption = "fill" | "fit" | "fill-down" | "force" | "auto";
 
+export interface ContrastAware {
+  useContrastColors?: boolean;
+}
+
 export * from "@thestory/standard-core/types/strapi";
 export * from "@thestory/standard-core/types/strapiTypes";
