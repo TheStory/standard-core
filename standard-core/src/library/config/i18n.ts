f9fcc3848438167f4ecbd7cdf9f147553117a1cd
Yaroslav Shatkevich <jarek@thestory.pl>
2025-03-13 13:13:47 +0100

    Add type annotation for localePrefix in i18n configuration

--- standard-core/src/library/config/i18n.ts
+++ standard-core/src/library/config/i18n.ts
@@ -1,7 +1,10 @@
+import type { LocalePrefix } from "next-intl/routing";
+
 export const defaultLocale = process.env.NEXT_PUBLIC_DEFAULT_LOCALE ?? "en";
 const defaultLocales = ["en", "pl"] as const;
 export const locales: readonly string[] =
   process.env.NEXT_PUBLIC_LOCALES?.split(",") ?? defaultLocales;
-export const localePrefix = process.env.NEXT_PUBLIC_LOCALE_PREFIX ?? "always";
+export const localePrefix = (process.env.NEXT_PUBLIC_LOCALE_PREFIX ??
+  "always") as LocalePrefix;
 
 export type Locales = (typeof locales)[number];
