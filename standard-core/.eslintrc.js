44eb7796700266eec69003c357b788c3c4850f41
Yaroslav Shatkevich <jarek@thestory.pl>
2025-03-19 15:15:29 +0100

    Add TypeScript ESLint plugin and update ESLint configuration

--- standard-core/.eslintrc.js
+++ standard-core/.eslintrc.js
@@ -2,8 +2,10 @@ const eol = require("os").platform();
 
 module.exports = {
   extends: ["next/core-web-vitals", "prettier", "plugin:storybook/recommended"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "@typescript-eslint/eslint-plugin"],
   rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-import-type-side-effects": "error",
     "prettier/prettier": ["warn", { endOfLine: "auto" }],
     "linebreak-style": ["warn", eol === "win32" ? "windows" : "unix"],
     "react/jsx-curly-brace-presence": ["warn", "never"],
