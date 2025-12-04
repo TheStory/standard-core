75a7200fb7a22cc7cf3abfd9601a76809c896925
Yaroslav Shatkevich <jarek@thestory.pl>
2025-12-04 13:25:37 +0100

    chore: update ESLint configuration with stricter rules and platform-specific settings

--- standard-core/.eslintrc.cjs
+++ standard-core/.eslintrc.cjs
@@ -3,21 +3,37 @@
  * - Runs independently of Next’s `next lint`
  * - Lints TS/TSX source and stories; ignores build outputs
  */
+const eol = require("os").platform();
+
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "prettier", "storybook"],
-  extends: [
-    "next",
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:storybook/recommended",
-  ],
+  extends: ["next/core-web-vitals", "prettier", "plugin:storybook/recommended"],
+  plugins: ["prettier", "@typescript-eslint/eslint-plugin"],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-import-type-side-effects": "error",
+    "prettier/prettier": ["warn", { endOfLine: "auto" }],
+    "linebreak-style": ["warn", eol === "win32" ? "windows" : "unix"],
+    "react/jsx-curly-brace-presence": ["warn", "never"],
+    "no-restricted-imports": [
+      "error",
+      {
+        paths: [
+          {
+            name: "@mui/material",
+            message:
+              "Importuj komponenty bezpośrednio, np. import Box from '@mui/material/Box'",
+          },
+        ],
+        patterns: ["@mui/material/*/*"],
+      },
+    ],
+  },
   ignorePatterns: [
     "dist/**",
     ".next/**",
     "storybook-static/**",
     "node_modules/**",
+    "src/library/types/strapiTypes/*",
   ],
 };
