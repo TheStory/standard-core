8535ecbe162a57bcb3a0567800f2b44703fd0132
jakubwozniak <kwozniak992@gmail.com>
2025-12-04 16:07:38 +0100

    TSC-4: Refactor Button component to simplify prop definitions and improve type safety

--- standard-core/src/library/atoms/Button/Button.tsx
+++ standard-core/src/library/atoms/Button/Button.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import type { ButtonOwnProps } from "@mui/material/Button";
 import MuiButton from "@mui/material/Button";
-import type { SxProps, Theme } from "@mui/material/styles";
+import type { ButtonProps as MuiButtonProps } from "@mui/material/Button";
 import { LocalizedLink } from "@the-story/standard-core/config/navigation";
-import type { APIString } from "@the-story/standard-core/types";
-import type { ButtonHTMLAttributes, ElementType, ReactNode } from "react";
+import type { HTMLAttributeAnchorTarget } from "react";
+
+type ButtonProps = MuiButtonProps & {
+  target?: HTMLAttributeAnchorTarget;
+};
 
 const Button = ({
   type = "button",
@@ -16,28 +18,13 @@ const Button = ({
   color = "primary",
   size = "large",
   disabled = false,
-  onClick = (e: any) => {},
-  onMouseEnter = (e: any) => {},
+  onClick,
+  onMouseEnter,
   component = LocalizedLink,
   endIcon = null,
   startIcon = null,
   sx,
-}: {
-  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
-  href?: APIString;
-  target?: string;
-  children?: ReactNode;
-  variant?: ButtonOwnProps["variant"];
-  color?: ButtonOwnProps["color"];
-  size?: ButtonOwnProps["size"];
-  disabled?: boolean;
-  onClick?: (e: any) => void;
-  onMouseEnter?: (e: any) => void;
-  component?: ElementType;
-  endIcon?: ReactNode;
-  startIcon?: ReactNode;
-  sx?: SxProps<Theme>;
-}) => (
+}: ButtonProps) => (
   <MuiButton
     type={type}
     href={href || undefined}
