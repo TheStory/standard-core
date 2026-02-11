e6f7b2d09c59a1588de9384c1fbff1ff53db9ddb
Yaroslav Shatkevich <jarek@thestory.pl>
2026-02-11 14:32:01 +0100

    fix(Divider): use theme-based color token for background styling

--- standard-core/src/library/atoms/Divider/Divider.tsx
+++ standard-core/src/library/atoms/Divider/Divider.tsx
@@ -8,7 +8,7 @@ const Divider = (props: DividerProps) => (
   <MuiDivider
     {...props}
     sx={{
-      backgroundColor: "rgba(0, 0, 0, 0.12)",
+      backgroundColor: "divider",
       ...props?.sx,
     }}
   />
